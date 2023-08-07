9aff994989824ffc8f33d9311c348a6b06406941
juvrajsb <10777524@polimi.it>
2023-08-07 17:40:43 +0200

    String compare

--- a.c
+++ a.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){    
     char input[1000];
+    int i=0;
     
     while(fgets(input, sizeof(input), stdin))
     {
-    printf("%s", input);
+        i=strcmp(input,"aggiungi-stazione");
+        //{
+            
+            printf("%d",i);
+       // }
+        printf("%s", input);
     }
-    return 0;
+     return 0;
 }
+   
