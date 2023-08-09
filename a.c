b421e8ae3e6363621baecc0363372769d6dd2606
Juvraj Singh Banwait <10777524@polimi.it>
2023-08-09 16:50:53 +0200

    funzioni

--- a.c
+++ a.c
@@ -3,21 +3,69 @@
 #include <string.h>
 
 int i=0,j=0,k=0,l=0,x=0; //li uso per testare
-
-
-void aggiungi_stazione(int distanza, int num_auto, int autonomia[], int d)
-{
-    struct {
+typedef struct {
         int distanza;
         int num_auto;
         int autonomia[100];
-    }stazione;
-    stazione.distanza=distanza;
-    stazione.num_auto=num_auto;
-    stazione.autonomia[d]=autonomia[d];
-    printf("%d %d %d \n", stazione.distanza,stazione.num_auto,stazione.autonomia[d]);
+}stazioni;
+void aggiungi_stazione(stazioni stazione[],int distanza, int num_auto, int autonomia[])
+{
+    if(stazione[distanza].distanza==0)
+    {
+    stazione[distanza].distanza=distanza;
+    stazione[distanza].num_auto=num_auto;
+    for(int d=0;d<num_auto;d++)
+    {
+        stazione[distanza].autonomia[d]=autonomia[d];
+        //printf("%d %d \n", stazione[distanza].distanza,stazione[distanza].autonomia[d]);
+    }
+    printf("aggiunta \n");
+    }
+    else
+    {
+        printf("non aggiunta\n");
+    }
+    ///printf("%d %d \n", stazione[distanza].distanza,stazione[distanza].autonomia[d]);
 }
-void analisi_input(char *input)
+void demolisci_stazione(stazioni stazione[], int distanza, int num_auto)
+{
+    if(stazione[distanza].distanza!=0)
+    {
+        stazione[distanza].distanza=-1;
+        stazione[distanza].num_auto=0;
+        printf("demolita\n");
+    }
+    else
+    {
+        printf("non demolita\n");
+    }
+    
+}
+void aggiungi_auto(stazioni stazione[],int distanza, int num_auto, int autonomia[])
+{
+    stazione[distanza].num_auto+=1;
+    stazione[distanza].autonomia[num_auto+1]=autonomia[num_auto+1];
+    printf("aggiunto\n");
+}
+void rottama_auto(stazioni stazione[], int distanza, int num_auto, int autonomia[])
+{
+    for(int d=0;d<num_auto;d++)
+    {
+        if(stazione[distanza].autonomia[d]==autonomia[d])
+        {
+            stazione[distanza].autonomia[d]=-1;
+            printf("%d %d \n", stazione[distanza].distanza,stazione[distanza].autonomia[d]);
+            break;
+        }
+        else{
+            printf("non rottamato");
+        }
+        printf("distanza %d  autonomia %d \n", stazione[distanza].distanza,stazione[distanza].autonomia[d]);
+    }
+    stazione[distanza].num_auto-=1;
+    printf("num auto %d \n", stazione[distanza].num_auto);
+}
+void analisi_input(char *input, stazioni stazione[])
 {
     char input_tagliato[20];
     int distanza=0, num_auto=0, autonomia[100];
@@ -38,32 +86,33 @@ void analisi_input(char *input)
                     input=strchr(input+1, ' '); 
                     //strchr() la uso per trovare il prossimo spazio in input, cosicche input poi punta allo spazio appena trovato
                     //printf("%d ",autonomia[d]);
-                    aggiungi_stazione(distanza, num_auto, autonomia, d);
+                    
                 }
                 //printf("Distanza %d \n", distanza);
                 //printf("Numero auto %d \n", num_auto);
-               
+               aggiungi_stazione(stazione,distanza, num_auto, autonomia);
             }
             if(strcmp(input_tagliato,"demolisci-stazione")==0)
             {
                 j++;
                 //printf("Demolisci: %d \n",j);
                 sscanf(input, "%*s %d", &distanza);
-                ///funzione che toglie la stazione
+                demolisci_stazione(stazione,distanza, num_auto);
+
             }
             if(strcmp(input_tagliato,"aggiungi-auto")==0)
             {
                 k++;
                 //printf("aggiungi auto: %d \n",k);
                 sscanf(input,"%*s %d %d", &distanza, &autonomia[k]);
-                /// funzione controlla se la stazione esiste e la aggiunge
+                aggiungi_auto(stazione,distanza, num_auto, autonomia);
             }
             if(strcmp(input_tagliato,"rottama-auto")==0)
             {
                 l++;
                 //printf("rottama: %d \n",l);
                 sscanf(input,"%*s %d %d", &distanza, &autonomia[l]);
-                /// funzione controlla se la stazione esiste e la aggiunge e controlla se esiste almeno un'auto con autonomia 
+                rottama_auto(stazione,distanza, num_auto, autonomia);
                 }
             if(strcmp(input_tagliato,"pianifica-percorso")==0)
             {
@@ -78,10 +127,10 @@ void analisi_input(char *input)
 }
 int main(){    
     char input[1000];
-    
+    stazioni stazione[1000];
     while(fgets(input, sizeof(input), stdin)!=NULL)
     {
-        analisi_input(input);
+        analisi_input(input, stazione);
     }
     /*printf("Demolisci: %d \n",j);
     printf("aggiungi auto: %d \n",k);
